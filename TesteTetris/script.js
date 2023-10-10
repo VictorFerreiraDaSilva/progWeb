3433b8cd9fbf4cdfb7a2f953073f6c80f990189b
Victor Ferreira da Silva <victor.ferreira.silva001@gmail.com>
2023-10-10 18:53:26 -0300

    Tetris: inserir peças 3 a 6 feito

--- TesteTetris/script.js
+++ TesteTetris/script.js
@@ -68,7 +68,6 @@ function verificarMaiorPosicaoLivre(coluna, qt = 1) {
   for (let i = 0; i < qt; i++) {
     l.push(-1);
     let mais = coluna + i;
-    mais = mais % 10;
     for (let j = 0; j < ln; j++) {
       if (jogo[j][mais] === caractereInvisivel) l[i]++;
       if (jogo[j][mais] != caractereInvisivel) break;
@@ -79,6 +78,9 @@ function verificarMaiorPosicaoLivre(coluna, qt = 1) {
 }
 
 function inserirPeca1(coluna) {
+  if (coluna === undefined) return;
+  if (coluna === null) return;
+  if (coluna < 0 || coluna > 9) return;
   let pos = verificarMaiorPosicaoLivre(coluna);
   for (let i = 0; i < 4; i++) {
     jogo[pos - i][coluna] = 'X';
@@ -87,13 +89,67 @@ function inserirPeca1(coluna) {
 }
 
 function inserirPeca2(coluna) {
+  if (coluna === undefined) return;
+  if (coluna === null) return;
+  if (coluna < 0 || coluna > 8) return;
   let mais = coluna + 1;
-  mais = mais % 10;
-  console.log(mais);
   let pos1 = verificarMaiorPosicaoLivre(coluna, 2);
   jogo[pos1][coluna] = 'Y';
   jogo[pos1 - 1][coluna] = 'Y';
   jogo[pos1][mais] = 'Y';
   jogo[pos1 - 1][mais] = 'Y';
+  atualizaJogo();
+}
+
+function inserirPeca3(coluna) {
+  if (coluna === undefined) return;
+  if (coluna === null) return;
+  if (coluna < 0 || coluna > 8) return;
+  let mais = coluna + 1;
+  let pos1 = verificarMaiorPosicaoLivre(coluna, 2);
+  jogo[pos1][coluna] = 'Z';
+  jogo[pos1 - 1][coluna] = 'Z';
+  jogo[pos1 - 2][coluna] = 'Z';
+  jogo[pos1][mais] = 'Z';
+  atualizaJogo();
+}
+
+function inserirPeca4(coluna) {
+  if (coluna === undefined) return;
+  if (coluna === null) return;
+  if (coluna < 0 || coluna > 8) return;
+  let mais = coluna + 1;
+  let pos1 = verificarMaiorPosicaoLivre(coluna, 2);
+  jogo[pos1][coluna] = 'A';
+  jogo[pos1][mais] = 'A';
+  jogo[pos1 - 1][mais] = 'A';
+  jogo[pos1 - 2][mais] = 'A';
+  atualizaJogo();
+}
+
+function inserirPeca5(coluna) {
+  if (coluna === undefined) return;
+  if (coluna === null) return;
+  if (coluna < 0 || coluna > 7) return;
+  let mais = coluna + 1;
+  let pos1 = verificarMaiorPosicaoLivre(coluna, 3);
+  jogo[pos1][coluna] = 'B';
+  jogo[pos1][mais] = 'B';
+  jogo[pos1 - 1][mais] = 'B';
+  jogo[pos1][++mais] = 'B';
+  atualizaJogo();
+}
+
+function inserirPeca6(coluna) {
+  if (coluna === undefined) return;
+  if (coluna === null) return;
+  if (coluna < 0 || coluna > 7) return;
+  let mais = coluna + 1;
+  let pos1 = verificarMaiorPosicaoLivre(coluna, 3);
+  jogo[pos1][coluna] = 'C';
+  jogo[pos1 - 1][coluna] = 'C';
+  jogo[pos1][mais] = 'C';
+  jogo[pos1][++mais] = 'C';
+  jogo[pos1 - 1][mais] = 'C';
   atualizaJogo();
 }
