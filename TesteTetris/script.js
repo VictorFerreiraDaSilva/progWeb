d205a6b145de982f5e4ca9389728effb34b667ec
Victor Ferreira da Silva <victor.ferreira.silva001@gmail.com>
2023-10-10 16:58:40 -0300

    funcao que apaga as linhas completas no fim e desce as outras

--- TesteTetris/script.js
+++ TesteTetris/script.js
@@ -1,11 +1,17 @@
 'use strict';
-let jogo = inicailizarMatriz(20, 10);
+const caractereInvisivel = '⠀';
+
+let linha = 20;
+let coluna = 10;
+
+let jogo = inicailizarMatriz(linha, coluna);
 console.log(jogo);
 console.log(verificarLinhaCompleta(jogo[0]));
-function inicializarArray(tamanho) {
+
+function inicializarArray(tamanho, vazio) {
   var array = [];
   for (let i = 0; i < tamanho; i++) {
-    array[i] = null;
+    array[i] = vazio === 1 ? caractereInvisivel : i;
   }
   return array;
 }
@@ -13,13 +19,39 @@ function inicializarArray(tamanho) {
 function inicailizarMatriz(linha, coluna) {
   var array = [];
   for (let i = 0; i < linha; i++) {
-    array.push(inicializarArray(coluna));
+    array.push(inicializarArray(coluna, 1));
   }
   return array;
 }
 
 function verificarLinhaCompleta(linha) {
   let contador = 0;
-  for (let i = 0; i < linha.length; i++) if (linha[i] != null) contador++;
+  for (let i = 0; i < linha.length; i++) {
+    if (linha[i] != caractereInvisivel) contador++;
+  }
   return contador === linha.length;
+}
+
+function completaLinha(linha) {
+  var array = inicializarArray(coluna, 0);
+  jogo[linha] = array;
+  atualizaJogo();
+}
+
+function atualizaJogo() {
+  for (var i = 0; i < linha; i++) {
+    for (var j = 0; j < coluna; j++) {
+      let celula = i.toString() + '_' + j.toString();
+      document.getElementById(celula).innerHTML = jogo[i][j];
+    }
+  }
+}
+
+function limparLinhas() {
+  while (verificarLinhaCompleta(jogo[19])) {
+    jogo.pop();
+    jogo.unshift(inicializarArray(coluna, 1));
+  }
+
+  atualizaJogo();
 }
