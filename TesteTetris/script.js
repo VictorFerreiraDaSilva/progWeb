707d556e0cf5b9ca78b01c7a689a2b08ce0bdd3f
Victor Ferreira da Silva <victor.ferreira.silva001@gmail.com>
2023-10-10 17:31:43 -0300

    Tetris: Inserir Peça 1 feito

--- TesteTetris/script.js
+++ TesteTetris/script.js
@@ -1,10 +1,10 @@
 'use strict';
 const caractereInvisivel = '⠀';
 
-let linha = 20;
-let coluna = 10;
+let ln = 20;
+let cl = 10;
 
-let jogo = inicailizarMatriz(linha, coluna);
+let jogo = inicailizarMatriz(ln, cl);
 console.log(jogo);
 console.log(verificarLinhaCompleta(jogo[0]));
 
@@ -33,14 +33,14 @@ function verificarLinhaCompleta(linha) {
 }
 
 function completaLinha(linha) {
-  var array = inicializarArray(coluna, 0);
+  var array = inicializarArray(cl, 0);
   jogo[linha] = array;
   atualizaJogo();
 }
 
 function atualizaJogo() {
-  for (var i = 0; i < linha; i++) {
-    for (var j = 0; j < coluna; j++) {
+  for (var i = 0; i < ln; i++) {
+    for (var j = 0; j < cl; j++) {
       let celula = i.toString() + '_' + j.toString();
       document.getElementById(celula).innerHTML = jogo[i][j];
     }
@@ -50,8 +50,24 @@ function atualizaJogo() {
 function limparLinhas() {
   while (verificarLinhaCompleta(jogo[19])) {
     jogo.pop();
-    jogo.unshift(inicializarArray(coluna, 1));
+    jogo.unshift(inicializarArray(cl, 1));
   }
 
+  atualizaJogo();
+}
+
+function verificarMaiorPosicaoLivre(coluna) {
+  let l = -1;
+  jogo.forEach((linha) => {
+    if (linha[coluna] === caractereInvisivel) l++;
+  });
+  return l;
+}
+
+function inserirPeca1(coluna) {
+  let pos = verificarMaiorPosicaoLivre(coluna);
+  for (let i = 0; i < 4; i++) {
+    jogo[pos - i][coluna] = 'X';
+  }
   atualizaJogo();
 }
