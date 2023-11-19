de105fb190f17754f2a5ccae1a8bb3dfc5468b74
Victor Ferreira da Silva <victor.ferreira.silva001@gmail.com>
2023-11-19 16:45:06 -0300

    FEAT: Tetris login

--- TetrisPHP/index.php
+++ TetrisPHP/index.php
@@ -20,9 +20,9 @@
     <form action="index.php" method="post">
     <section class="caixa_start">
       <h2 class="texto">USUÁRIO</h2>
-      <input class="input" id="nome" />
+      <input class="input" id="nome" name="usuario"/>
       <h2 class="texto">SENHA</h2>
-      <input class="input" id="senha" type="password" />
+      <input class="input" id="senha" type="password" name="senha"/>
       <input type="submit" name="login" value="JOGAR" class="botao_jogar">
       <section class="register">
         <h2>Primeira vez jogando?</h2>
@@ -37,16 +37,47 @@
 </html>
 
 <?php
+  include 'System/db.php';
+
+  $dbi = new DataBaseInfo();
+  
+  $servername = $dbi->getServerName(); 
+  $username = $dbi->getUsername();
+  $password = $dbi->getPassword();
+  $db = $dbi->getDB();
 
   if(isset($_POST["login"])){
-     if(!empty($_POST["usuario"]) && !empty($_POST["senha"])){
-      $_SESSION["usuario"] = $_POST["usuario"];
-      $_SESSION["senha"] = $_POST["senha"];    }
+    if(!empty($_POST["usuario"]) && !empty($_POST["senha"])){
 
-      header("Location: tabuleiro.php");
+      $conn = new mysqli($servername, $username, $password, $db);
+      // Check connection
+      if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+      }
+
+      $sql = "SELECT username, nome, dt_nasc, cpf, telefone, email FROM usuario where username = '" . $_POST["usuario"] . "' and senha = md5('" . $_POST["senha"] ."');";
+      $result = $conn->query($sql);
+      if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+          $_SESSION["usuario"] = $row["username"];
+          $_SESSION["nome"] = $row["nome"];
+          $_SESSION["dt_nasc"] = $row["dt_nasc"];
+          $_SESSION["cpf"] = $row["cpf"];
+          $_SESSION["telefone"] = $row["telefone"];
+          $_SESSION["email"] = $row["email"];
+        }
+        $conn->close();   
+    
+        header("Location: tabuleiro.php");
+      } else {
+        $conn->close();   
+        echo "Usuario e/ou Senha inválidos";
+      }
     }
-    else{
-        echo"Está faltando usuário/senha <br>";
+  }
+  else{
+    echo"Está faltando usuário/senha <br>";
   }
 
 ?>
